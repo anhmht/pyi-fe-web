9d933c452f9fcee0e382abbee8af732654923c96
Tri Anh <anhmht@gmail.com>
2022-05-25 21:36:44 +0700

    init source FE

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Button> Trss</Button>
+    <Tutorial />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Button } from 'element-ui'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  components: {
+    Button
+  }
+})
+</script>
