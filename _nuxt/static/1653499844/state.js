e8b580e17f6380860da69c5e12ca807486f68ce4
anhmht <anhmht@gmail.com>
2022-05-25 17:30:54 +0000

    [skip ci] Updates

---
+++ _nuxt/static/1653499844/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpyi-fe-web\u002F_nuxt\u002Fstatic\u002F1653499844",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fpyi-fe-web\u002F",assetsPath:"\u002Fpyi-fe-web\u002F_nuxt\u002F",cdnURL:a}}}}(null));
