0d69204f6748146d7c8d77d01e4198824910cafa
Tri Anh <anhmht@gmail.com>
2022-06-02 01:14:02 +0700

    update nuxt cfonig

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,10 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/element-ui', '@/plugins/scroll-animation'],
+  plugins: [
+    '~/plugins/element-ui',
+    { src: '~/plugins/scroll-animation', mode: 'client' }
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
