1c11ebe5d327c8278685403d6d6037a70c38ae5f
Tri Anh <anhmht@gmail.com>
2022-07-04 21:55:04 +0700

    add search query

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -1,25 +1,13 @@
 <template>
   <div :class="$style.root">
     <div :class="$style.wrapper">
-      <!-- <div :class="$style.logo">
-        <img src="~/assets/images/logo.png" width="80" />
-      </div> -->
       <h1>What are you looking for?</h1>
-      <el-autocomplete
-        :class="$style.search"
-        v-model="search"
-        :fetch-suggestions="querySearch"
-        placeholder="Type to search"
-        :trigger-on-focus="false"
-        @select="handleSelect"
-      >
-        <i slot="suffix" class="fa fa-search"></i>
-      </el-autocomplete>
+      <SearchInput :query.sync="search" />
       <div :class="$style.footer">
-        <nuxt-link to="/cateogry/women">Women</nuxt-link>
-        <nuxt-link to="/cateogry/men">Men</nuxt-link>
-        <nuxt-link to="/cateogry/accessories">Accessories</nuxt-link>
-        <nuxt-link to="/cateogry/home-living">Home & Living</nuxt-link>
+        <nuxt-link to="/category/women">Women</nuxt-link>
+        <nuxt-link to="/category/men">Men</nuxt-link>
+        <nuxt-link to="/category/accessories">Accessories</nuxt-link>
+        <nuxt-link to="/category/home-living">Home & Living</nuxt-link>
       </div>
     </div>
   </div>
@@ -27,48 +15,46 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import SearchInput from '~/components/pages/search/SearchInput.vue'
+import { Product } from '~/model/product/product'
 
 export default Vue.extend({
+  components: { SearchInput },
   data(): {
-    links: Object[]
     search: string
+    result: Product[]
   } {
     return {
-      links: [],
-      search: ''
+      search: '',
+      result: []
     }
+  },
+  computed: {
+    queryFilters(): string | undefined {
+      return this.$route.query.text as string
+    }
+  },
+  created() {
+    this.search = this.queryFilters ? this.queryFilters : ''
+    console.log(this.search)
   },
   methods: {
-    querySearch(queryString, cb) {
-      var links = this.links
-      var results = queryString
-        ? links.filter(this.createFilter(queryString))
-        : links
-      // call callback function to return suggestions
-      cb(results)
-    },
-    createFilter(queryString) {
-      return (link) => {
-        return link.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0
-      }
-    },
-    loadAll() {
-      return [
-        { value: 'vue', link: 'https://github.com/vuejs/vue' },
-        { value: 'element', link: 'https://github.com/ElemeFE/element' },
-        { value: 'cooking', link: 'https://github.com/ElemeFE/cooking' },
-        { value: 'mint-ui', link: 'https://github.com/ElemeFE/mint-ui' },
-        { value: 'vuex', link: 'https://github.com/vuejs/vuex' },
-        { value: 'vue-router', link: 'https://github.com/vuejs/vue-router' },
-        { value: 'babel', link: 'https://github.com/babel/babel' }
-      ]
-    },
-    handleSelect(item) {
-      console.log(item)
-    }
+    searchKeyword(): void {}
   },
-  mounted() {
-    this.links = this.loadAll()
+  watch: {
+    search(value: string) {
+      this.$router
+        .replace({
+          query: {
+            text: value
+          }
+        })
+        .catch((error: Error) => {
+          if (error.name !== 'NavigationDuplicated') {
+            throw error
+          }
+        })
+    }
   }
 })
 </script>
@@ -83,30 +69,8 @@ export default Vue.extend({
   .wrapper {
     width: 450px;
   }
-  .logo {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
   h1 {
     color: #fff;
-  }
-  .search {
-    width: 100%;
-    margin-top: var(--space-2x);
-  }
-  :global(.el-input__suffix) {
-    display: flex;
-    align-items: center;
-    right: var(--space);
-    input {
-      border-radius: 40px;
-      height: 50px;
-    }
-  }
-  :global(.el-input__inner) {
-    border-radius: 40px;
-    height: 50px;
   }
 
   .footer {
