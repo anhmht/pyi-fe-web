99c262d97ab6506ea0085d7ddd8f2331b04f310e
anhmht <anhmht@gmail.com>
2022-07-13 16:09:48 +0000

    [skip ci] Updates

--- _nuxt/static/1657727931/forgot-password/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpyi-fe-web\u002F_nuxt\u002Fstatic\u002F1657727931",layout:"default",error:a,state:{currentUser:void 0,shoppingCart:[]},serverRendered:true,routePath:"\u002Fforgot-password",config:{_app:{basePath:"\u002Fpyi-fe-web\u002F",assetsPath:"\u002Fpyi-fe-web\u002F_nuxt\u002F",cdnURL:a}}}}(null));
