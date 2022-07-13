e27c533960a80d6cb1ded1a6b62d6ea8ac61d8a1
anhmht <anhmht@gmail.com>
2022-07-13 15:59:05 +0000

    [skip ci] Updates

--- _nuxt/static/1657299824/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpyi-fe-web\u002F_nuxt\u002Fstatic\u002F1657299824",layout:"default",error:a,state:{currentUser:void 0},serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fpyi-fe-web\u002F",assetsPath:"\u002Fpyi-fe-web\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1657727931/error/not-found/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpyi-fe-web\u002F_nuxt\u002Fstatic\u002F1657727931",layout:"default",error:a,state:{currentUser:void 0,shoppingCart:[]},serverRendered:true,routePath:"\u002Ferror\u002Fnot-found",config:{_app:{basePath:"\u002Fpyi-fe-web\u002F",assetsPath:"\u002Fpyi-fe-web\u002F_nuxt\u002F",cdnURL:a}}}}(null));
