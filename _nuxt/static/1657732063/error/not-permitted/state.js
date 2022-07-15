5297d723d2b59126e999c4bfe37bc5a580830c41
anhmht <anhmht@gmail.com>
2022-07-15 16:57:43 +0000

    [skip ci] Updates

--- _nuxt/static/1657732063/error/not-permitted/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpyi-fe-web\u002F_nuxt\u002Fstatic\u002F1657732063",layout:"default",error:a,state:{currentUser:void 0,shoppingCart:[]},serverRendered:true,routePath:"\u002Ferror\u002Fnot-permitted",config:{_app:{basePath:"\u002Fpyi-fe-web\u002F",assetsPath:"\u002Fpyi-fe-web\u002F_nuxt\u002F",cdnURL:a}}}}(null));
