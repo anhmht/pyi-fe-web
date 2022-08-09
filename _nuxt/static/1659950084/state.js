6e12ebb84041b2880cfcf7ae4c34bc33df6b5f51
anhmht <anhmht@gmail.com>
2022-08-09 11:12:17 +0000

    [skip ci] Updates

--- _nuxt/static/1659950084/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659950084",layout:"default",error:a,state:{currentUser:void 0,shoppingCart:[]},serverRendered:false,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
